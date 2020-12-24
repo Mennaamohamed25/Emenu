9fb943d391022d06d5367cd7926215d1631b172e
Mennamuhamedd <mennamuhamed20@yahoo.com>
2020-12-24 21:38:20 +0200

    setup

---
+++ js/plugin.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("h1").fadeOut();
+    });
+  });
+  /* */
+  // $(document).ready(function(){  		
+  //   $(".container-fluid .form-box .text-end .next").click(function(){
+  //     $(this).parents(".container-fluid .form-box ").fadeOut('fast');
+  //     $(this).parents("active-button").next().fadeIn('fast');
+  //   });
+  //   $(".multistep-container .form-box .button-row .previous").click(function(){
+  //     $(this).parents(".form-box").fadeOut('fast');
+  //     $(this).parents(".form-box").prev().fadeIn('fast');
+  //   });
+  
+  // });

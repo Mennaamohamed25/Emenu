1cfd460558ec1e90a9df0607e8ec64a571d50b36
Mennamuhamedd <mennamuhamed20@yahoo.com>
2020-12-28 14:52:18 +0200

    new

--- js/plugin.js
+++ js/plugin.js
@@ -82,15 +82,4 @@ $(document).ready(function(){
   });
   
   });
-  /* */
-  // $(document).ready(function(){  		
-  //   $(".container-fluid .form-box .text-end .next").click(function(){
-  //     $(this).parents(".container-fluid .form-box ").fadeOut('fast');
-  //     $(this).parents("active-button").next().fadeIn('fast');
-  //   });
-  //   $(".multistep-container .form-box .button-row .previous").click(function(){
-  //     $(this).parents(".form-box").fadeOut('fast');
-  //     $(this).parents(".form-box").prev().fadeIn('fast');
-  //   });
-  
-  // });
+
